f2a2e0c8681a0d27a5f64e6b12431305eba4a678
578593908@qq.com <5841314520>
2018-07-10 23:57:48 +0800

    add

--- src/views/app.vue
+++ src/views/app.vue
@@ -1,13 +1,32 @@
-<style></style>
-<template>
-	<div>ab11c</div>
-</template>
-<script >
-	export default {
-		data(){
-			return {
+<style type="text/scss" lang="sass">
+    @import "../sass/common";
+    $width: 400px;
+    #app_div {
+        .border {
+            border: 1px solid red;
+        }
+        .div1 {
+            width: $width;
+            height: 300px;
+        }
+        span {
 
-			}
-		}
-	}
+        }
+
+    }
+</style>
+<template>
+    <div id="app_div">
+        <div class="border div1">
+
+        </div>
+        <span class="span_c">span</span>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    }
 </script>
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,38 @@
-const WebpackDevServerOutput = require('webpack-dev-server-output');
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const path = require('path')
 module.exports = {
-	mode: 'development',
-	devtool: 'eval-source-map',
-  	entry:  __dirname + "/src/index.js",//已多次提及的唯一入口文件
-  	output: {
-	    path: __dirname + "/dist",//打包后的文件存放的地方
-	    filename: "bundle.js"//打包后输出文件的文件名
-	},
-	module:{
-		rules:[
-			// {
-			// 	test:/.js$/,
-			// 	loader:"babel-loader",
-			// 	exclude: "/node_modules"
-			// },
-			{
+    mode: 'development',
+    devtool: 'eval-source-map',
+    entry: __dirname + "/src/index.js",//已多次提及的唯一入口文件
+    output: {
+        path: __dirname + "/dist",//打包后的文件存放的地方
+        filename: "bundle.js"//打包后输出文件的文件名
+    },
+    module: {
+        rules: [
+            // {
+            // 	test:/.js$/,
+            // 	loader:"babel-loader",
+            // 	exclude: "/node_modules"
+            // },
+            {
                 test: /.vue$/,
-                loader: 'vue-loader'
-            }
-		]
-	},
-	plugins:[
-		// new WebpackDevServerOutput({
-		// 	path: '/dist',
-		// 	isDel: true
-		// }),
-		new VueLoaderPlugin()
-	],
-	resolve: { alias: { 'vue': 'vue/dist/vue.js' } }
+                loader: 'vue-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.sass$/,
+                loader: ['vue-style-loader', 'css-loader', 'sass-loader']
+            },
+
+        ]
+    },
+    plugins: [
+        // new WebpackDevServerOutput({
+        // 	path: '/dist',
+        // 	isDel: true
+        // }),
+        new VueLoaderPlugin()
+    ],
+    resolve: {alias: {'vue': 'vue/dist/vue.js'}}
 
 }
